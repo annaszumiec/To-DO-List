fba38f7fd965a0817840e3a0cc8f5c0506577bba
annamaria <szumiecam@gmail.com>
2022-12-01 10:39:09 +0100

    List function

---
+++ script.js
@@ -0,0 +1,29 @@
+$('button').addClass('red');
+$('button').addClass('text--white');
+
+$('button').click(
+    function(){
+        let toAdd = $('#input').val();
+    
+         if($('#input').val() ===''){
+            alert("Write item to add to the list")
+         }else{
+            $('ol').append('<li>' + toAdd + '</li>');
+         }
+
+         $('#input[type="text"]').val('');
+
+    });
+
+$('ol').sortable();  
+
+$('ol').on('click','li', function(){
+    $(this).toggleClass('strike');    
+  });
+
+$('ol').on('dblclick','li', function(){
+     $(this).toggleClass('strike').fadeOut('slow');  
+  });
+
+
+
